8b6842cdc33ed03eeeb42bd4089d6fb3241ba5fb
tanjonaaa <hei.tanjona@gmail.com>
2024-05-07 21:59:45 +0300

    chore: database access config

---
+++ src/main/java/org/lasantsy/lasantsy/repository/config/DbConnector.java
@@ -0,0 +1,26 @@
+package org.lasantsy.lasantsy.repository.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Configuration
+public class DbConnector {
+    private final DbProperties dbProperties;
+
+    public DbConnector(DbProperties dbProperties) {
+        this.dbProperties = dbProperties;
+    }
+
+    @Bean
+    public Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                dbProperties.getUrl(),
+                dbProperties.getUser(),
+                dbProperties.getPassword()
+        );
+    }
+}
---
+++ src/main/java/org/lasantsy/lasantsy/repository/config/DbProperties.java
@@ -0,0 +1,18 @@
+package org.lasantsy.lasantsy.repository.config;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+@ConfigurationProperties("db")
+public class DbProperties {
+    private String url;
+    private String user;
+    private String password;
+}
