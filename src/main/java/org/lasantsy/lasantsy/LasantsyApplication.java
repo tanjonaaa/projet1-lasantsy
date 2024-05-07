47b7619df7747e329e12a74f94dc20cde3aa2a72
tanjonaaa <hei.tanjona@gmail.com>
2024-05-07 20:43:15 +0300

    feat: spring initialization

---
+++ src/main/java/org/lasantsy/lasantsy/LasantsyApplication.java
@@ -0,0 +1,13 @@
+package org.lasantsy.lasantsy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LasantsyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LasantsyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/lasantsy/lasantsy/LasantsyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lasantsy.lasantsy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LasantsyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
